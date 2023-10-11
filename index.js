692c74048fcb285bddbbbf607cc02949774421a1
Chen, Chang-Min <47084071+changmin-chen@users.noreply.github.com>
2023-10-12 01:00:00 +0800

    homepage hamburger nav

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+ hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+
+    navMenu.classList.toggle("invisible")
+    navMenu.classList.toggle("flex");
+ })
+
+ document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
